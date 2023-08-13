7b885c12631d5143a76532f4d038237596194d80
Henrique Cardoso <henrique.jc2016@gmail.com>
2023-08-13 14:33:49 -0300

    reutilizando o script navbar

---
+++ js/modelos/navbar.js
@@ -0,0 +1,30 @@
+export default function initNavBar() {
+  const hamburger = document.querySelector('.hamburger');
+  const headerMenu = document.querySelector('.header-menu');
+  var ultimoScroll = 0;
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    headerMenu.classList.toggle('active');
+  });
+
+  document.querySelectorAll('.nav-link').forEach((n) =>
+    n.addEventListener('click', () => {
+      hamburger.classList.remove('active');
+      headerMenu.classList.remove('active');
+    })
+  );
+
+  window.addEventListener('scroll', function () {
+    var atualScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (atualScroll > ultimoScroll) {
+      if (headerMenu.classList.contains('active')) {
+        headerMenu.classList.remove('active');
+        hamburger.classList.remove('active');
+      }
+    }
+
+    ultimoScroll = atualScroll;
+  });
+}
