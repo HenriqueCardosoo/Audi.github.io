25663768e5be74c60f87179d3059436720dbc3ff
Henrique Cardoso <henrique.jc2016@gmail.com>
2023-08-14 19:48:17 -0300

    selecionando o elemento certo do DOM

--- js/modelos/filter.js
+++ js/modelos/filter.js
@@ -1,5 +1,5 @@
 export default function initFilter() {}
-const filter = document.querySelector('.modelos-container');
+const filter = document.querySelector('.filtro-modelos');
 
 filter.addEventListener('click', () => {
   filter.classList.toggle('ativo');
