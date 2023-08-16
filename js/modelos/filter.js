06986c7e236d7325007d34b1b73dd49901b08ebc
Henrique Cardoso <henrique.jc2016@gmail.com>
2023-08-15 23:55:27 -0300

    adicionei animação no svg, mudei o nome para "Fechar Filtro" quando a gaveta do filtro estiver aberta(ativa)

--- js/modelos/filter.js
+++ js/modelos/filter.js
@@ -1,7 +1,17 @@
-export default function initFilter() {}
-const filter = document.querySelector('.filtro-modelos');
+export default function initFilter() {
+  const filter = document.querySelector('.filtro-modelos');
+  const filterModelos = document.querySelector('.filtro-bg');
 
-filter.addEventListener('click', () => {
-  filter.classList.toggle('ativo');
-  console.log('foi');
-});
+  filter.addEventListener('click', () => {
+    filter.classList.toggle('ativo');
+    filterModelos.classList.toggle('ativo');
+
+    const texto = filter.querySelector('p');
+
+    if (texto.textContent === 'Abrir Filtro') {
+      texto.textContent = 'Fechar Filtro';
+    } else {
+      texto.textContent = 'Abrir Filtro';
+    }
+  });
+}
