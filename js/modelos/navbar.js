b5fd61aa731e6aaf076f05a5dea2fde4a54df7c8
Henrique Cardoso <henrique.jc2016@gmail.com>
2023-09-14 00:53:09 -0300

    update em variaveis

--- js/modelos/navbar.js
+++ js/modelos/navbar.js
@@ -1,30 +1,30 @@
-export default function initNavBar() {
-  const hamburger = document.querySelector('.hamburger');
-  const headerMenu = document.querySelector('.header-menu');
-  var ultimoScroll = 0;
+// export default function initNavBar() {
+//   const hamburger = document.querySelector('.hamburger');
+//   const headerMenu = document.querySelector('.header-menu');
+//   const ultimoScroll = 0;
 
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    headerMenu.classList.toggle('active');
-  });
+//   hamburger.addEventListener('click', () => {
+//     hamburger.classList.toggle('active');
+//     headerMenu.classList.toggle('active');
+//   });
 
-  document.querySelectorAll('.nav-link').forEach((n) =>
-    n.addEventListener('click', () => {
-      hamburger.classList.remove('active');
-      headerMenu.classList.remove('active');
-    })
-  );
+//   document.querySelectorAll('.nav-link').forEach((n) =>
+//     n.addEventListener('click', () => {
+//       hamburger.classList.remove('active');
+//       headerMenu.classList.remove('active');
+//     })
+//   );
 
-  window.addEventListener('scroll', function () {
-    var atualScroll = window.pageYOffset || document.documentElement.scrollTop;
+//   window.addEventListener('scroll', function () {
+//     var atualScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-    if (atualScroll > ultimoScroll) {
-      if (headerMenu.classList.contains('active')) {
-        headerMenu.classList.remove('active');
-        hamburger.classList.remove('active');
-      }
-    }
+//     if (atualScroll > ultimoScroll) {
+//       if (headerMenu.classList.contains('active')) {
+//         headerMenu.classList.remove('active');
+//         hamburger.classList.remove('active');
+//       }
+//     }
 
-    ultimoScroll = atualScroll;
-  });
-}
+//     ultimoScroll = atualScroll;
+//   });
+// }
