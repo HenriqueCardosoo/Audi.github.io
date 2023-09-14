1551e5b379965368715d07f4cbc0a28c6336b727
Henrique Cardoso <henrique.jc2016@gmail.com>
2023-09-13 23:49:09 -0300

    alguns ajustes utilizado o eslint

--- js/plugins/navbar.js
+++ js/plugins/navbar.js
@@ -1,22 +1,22 @@
 export default function initNavBar() {
   const hamburger = document.querySelector('.hamburger');
   const headerMenu = document.querySelector('.header-menu');
-  var ultimoScroll = 0;
+  let ultimoScroll = 0;
 
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     headerMenu.classList.toggle('active');
   });
 
-  document.querySelectorAll('.nav-link').forEach(n =>
+  document.querySelectorAll('.nav-link').forEach((n) =>
     n.addEventListener('click', () => {
       hamburger.classList.remove('active');
       headerMenu.classList.remove('active');
     })
   );
 
-  window.addEventListener('scroll', function () {
-    var atualScroll = window.pageYOffset || document.documentElement.scrollTop;
+  window.addEventListener('scroll', () => {
+    const atualScroll = window.pageYOffset || document.documentElement.scrollTop;
 
     if (atualScroll > ultimoScroll) {
       if (headerMenu.classList.contains('active')) {
