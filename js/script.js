c448ae8bed14f4ee055fa63dc54e437df70aad46
Henrique Cardoso <henrique.jc2016@gmail.com>
2024-02-14 18:41:08 -0300

    add initSongMotor

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initAnimacao from './plugins/animacao.js';
 import initAnimaNumeros from './plugins/anima-numeros.js';
 import initPerformace from './plugins/performace.js';
 import initNavBar from './plugins/navbar.js';
+import initSongMotor from './plugins/songMotor.js';
 
 initAnimacao();
 initAnimaNumeros();
 initPerformace();
 initNavBar();
+initSongMotor();
